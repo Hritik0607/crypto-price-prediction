f9935edc3f5e4d7fe604b0ffad5449b35d6db5f9
Hritik0607 <hritikjaiswal6798@gmail.com>
2025-12-20 20:03:51 +0530

    fixed trades error

--- services/trades/kraken_api/trade.py
+++ services/trades/kraken_api/trade.py
@@ -11,15 +11,37 @@ class Trade(BaseModel):
     pair: str
     price: float
     volume: float
-    timestamp: datetime
+    timestamp: str
     timestamp_ms: int
 
-    @property
-    def timestamp_ms(self) -> int:
-        return int(self.timestamp.timestamp() * 1000)
+    @classmethod
+    def from_kraken_rest_api_response(
+        cls,
+        pair: str,
+        price: float,
+        volume: float,
+        timestamp: str,
+    ) -> 'Trade':
+        return cls(
+            pair=pair,
+            price=price,
+            volume=volume,
+            timestamp=timestamp,
+            timestamp_ms=cls._datestr2milliseconds(timestamp),
+        )
+
+    @staticmethod
+    def _datestr2milliseconds(datestr: str) -> int:
+        return int(
+            datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S.%fZ').timestamp() * 1000
+        )
+
+    def to_str(self) -> str:
+        # pydantic method to convert the model to a dict
+        return self.model_dump_json()
 
     def to_dict(self) -> dict:
-        return self.model_dump_json()
+        return self.model_dump()
 
     # def to_dict(self) -> dict:
     #     return {
